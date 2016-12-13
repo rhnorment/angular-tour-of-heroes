93a4e07af4ac7fd610e9bfbe67a1ec5d8eb68b4a
Hunt Norment <hunt@sherpa.red>
2016-12-12 18:33:26 -0600

    add hero detail html

--- app/app.module.ts
+++ app/app.module.ts
@@ -20,13 +20,17 @@ import { HeroService } from './hero.service';
         pathMatch: 'full'
       },
       {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
         path: 'heroes',
         component: HeroesComponent
       },
       {
-        path: 'dashboard',
-        component: DashboardComponent
-      }
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
     ])
   ],
   declarations: [
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,15 +1,22 @@
 /**
  * Created by rhnorment on 12/4/16.
  */
- import { Component, Input } from '@angular/core';
+ import 'rxjs/add/operator/switchMap';
+ import { Component, Input, OnInit } from '@angular/core';
+ import { ActivatedRoute, Params } from '@angular/router';
+ import { Location } from '@angular/common';
+ 
  import { Hero } from './hero';
+ import { HeroService } from './hero.service';
 
  @Component({
  	selector: 'my-hero-detail',
  	template: `
- 	<div *ngIf="hero">
+ 	  <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+      	<label>id: </label>{{hero.id}}
+    	</div>
       <div>
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
@@ -17,7 +24,22 @@
     </div>
     `
  })
- export class HeroDetailComponent {
- 	@Input()
- 	hero: Hero;
+ export class HeroDetailComponent implements OnInit {
+ 	@Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
  }
