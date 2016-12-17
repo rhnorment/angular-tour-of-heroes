05679f00842e671899c0f1caac67f84604fd2536
Hunt Norment <hunt@sherpa.red>
2016-12-17 10:37:10 -0600

    delete hero

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -29,10 +29,20 @@ export class HeroesComponent implements OnInit {
 
     if (!name) { return; }
 
-    this.heroService.create(name)
+    this.heroService
+      .create(name)
       .then(hero => {
         this.heroes.push(hero);
         this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
       });
   }
 
