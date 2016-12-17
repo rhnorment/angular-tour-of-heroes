15bf7559d2df37d11fb1a645bc5cdec227e166b8
Hunt Norment <hunt@sherpa.red>
2016-12-17 16:45:01 -0600

    add hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
+
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
 import { Hero } from './hero';
 
