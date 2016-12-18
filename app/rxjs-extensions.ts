5cbebca37059046708408ae291839cbd0a3ad680
Hunt Norment <hunt@sherpa.red>
2016-12-18 09:48:09 -0600

    final

--- app/rxjs-extensions.ts
+++ app/rxjs-extensions.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/observable/throw';
 // Observable operators
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distincUntilChanged';
+import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
